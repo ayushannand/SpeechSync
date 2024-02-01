6255f85e347bc504f45850ca5fedf35d56a8106f
Ayush Anand <direct.ayush@gmail.com>
2024-02-01 13:59:50 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import whisper
+
+import ssl
+
+# Disable SSL certificate verification (use with caution)
+ssl._create_default_https_context = ssl._create_unverified_context
+
+model = whisper.load_model("base")
+result = model.transcribe("audio/hi9.wav", fp16=False)
+print(result["text"])
