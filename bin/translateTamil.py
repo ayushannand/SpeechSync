efb6b286d345085c7540f4251a80e8bf05995e16
Ayush Anand <direct.ayush@gmail.com>
2024-04-01 23:31:05 +0530

    April Fool Changes

---
+++ bin/translateTamil.py
@@ -0,0 +1,22 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+
+def getTamil(lines): 
+    # Load translation model
+    tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-dra")
+    model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-dra")
+
+    # Translate each line using the Helsinki model
+    result = []
+    for line in lines:
+        # Translate the line using the model
+        inputs = tokenizer(line, return_tensors="pt", padding=True, truncation=True)
+        translated_line = model.generate(**inputs)
+        
+        # Decode the translated output
+        translated_text = tokenizer.decode(translated_line[0], skip_special_tokens=True)
+        
+        # Append the translated text to the result array
+        result.append(translated_text)
+
+    return result
